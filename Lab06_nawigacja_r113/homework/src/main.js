f2e37fa70e3297e4cdb88a42acc7f637e5bad964
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2022-12-27 23:37:47 +0100

    Add lib files, start working on lab6

---
+++ Lab06_nawigacja_r113/homework/src/main.js
@@ -0,0 +1,5 @@
+import * as THREE from '../lib/build/three.module.js';
+import Stats from "../lib/jsm/libs/stats.module.js";
+import { OrbitControls } from "../lib/jsm/controls/OrbitControls.js";
+import { FBXLoader } from "../lib/jsm/loaders/FBXLoader.js";
+
